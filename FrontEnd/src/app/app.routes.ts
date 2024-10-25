1bacdd917cd11d6a7d789e171717c5501d29cbf1
Juajsia <juanjo.jjev@gmail.com>
2024-10-24 20:08:25 -0500

    login, search and create froms created

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    title: 'Login',
+    path: "",
+    component: LoginComponent
+}, {
+    title: 'Menu',
+    path: "menu",
+    component: MenuComponent
+}];
