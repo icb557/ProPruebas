6361c202e64925fef8debbbf149af5c92614c1e4
cbisac <cortesbuitragoisac@gmail.com>
2024-12-19 16:43:22 -0500

    add bug for sonar

--- BackEnd/src/middleware/validateToken.js
+++ BackEnd/src/middleware/validateToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 
 export const validateToken = (req, res, next) => {
   const headerToken = req.get('authorization')
-  if (headerToken !== undefined && headerToken.startsWith('Bearer ')) {
+  if (headerToken != undefined && headerToken.startsWith('Bearer ')) {
     try {
       const token = headerToken.slice(7)
       jwt.verify(token, process.env.SECRET_KEY)
